60301d917fed9d6c30dba74f96fb4575b8c74bd5
Yannick Marcon <yannick.marcon@obiba.org>
2019-11-18 12:36:13 +0100

    Fix case = opertor is used in function parameters

--- ds4j-r/src/test/java/org/obiba/datashield/r/expr/DataShieldExprTest.java
+++ ds4j-r/src/test/java/org/obiba/datashield/r/expr/DataShieldExprTest.java
@@ -9,64 +9,62 @@
  */
 package org.obiba.datashield.r.expr;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.obiba.datashield.core.DSEnvironment;
+import org.obiba.datashield.core.DSMethod;
+import org.obiba.datashield.core.impl.DefaultDSEnvironment;
+import org.obiba.datashield.core.impl.PackagedFunctionDSMethod;
 
 import java.io.StringReader;
-import java.util.AbstractMap;
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class DataShieldExprTest {
 
-  private static final Map<String, String> tests = Collections.unmodifiableMap(Stream.of(
-      new AbstractMap.SimpleEntry<>("A symbol", "A"),
-      new AbstractMap.SimpleEntry<>("A number", "1.0"),
-      new AbstractMap.SimpleEntry<>("An integer", "5L"),
-      new AbstractMap.SimpleEntry<>("A negative number", "-0.43151402098822"),
-      new AbstractMap.SimpleEntry<>("An embedded symbol", "A$B$C.D"),
-      new AbstractMap.SimpleEntry<>("A subset symbol", "A[2,1]"),
-      new AbstractMap.SimpleEntry<>("An open row subset symbol", "A[,1]"),
-      new AbstractMap.SimpleEntry<>("An open column subset symbol", "A[1,]"),
-      new AbstractMap.SimpleEntry<>("A subset symbol with range", "A[,1:2]"),
-      new AbstractMap.SimpleEntry<>("A subset symbol with function call", "D[,func(D[,1])]"),
-      new AbstractMap.SimpleEntry<>("A subset symbol with spaces", "A[ , 1 ]"),
-      new AbstractMap.SimpleEntry<>("An empty subset symbol", "A[]"),
-      new AbstractMap.SimpleEntry<>("An almost empty subset symbol", "A[,]"),
-      new AbstractMap.SimpleEntry<>("A subset value symbol", "A[[1]]"),
-      new AbstractMap.SimpleEntry<>("A formula", "A ~ B"),
-      new AbstractMap.SimpleEntry<>("A function invocation", "A()"),
-      new AbstractMap.SimpleEntry<>("An operator on symbols", "A + B"),
-      new AbstractMap.SimpleEntry<>("Operator chaining", "A + B * C"),
-      new AbstractMap.SimpleEntry<>("Operator on functions", "A() + B * C"),
-      new AbstractMap.SimpleEntry<>("A formula with operators", "A ~ B + (C * D)^4 : E %in% F"),
-      new AbstractMap.SimpleEntry<>("Grouping", "(A + B) * (C - D)"),
-      new AbstractMap.SimpleEntry<>("Function with a single parameter", "A(B)"),
-      new AbstractMap.SimpleEntry<>("Function with a subset parameter", "A(B[,2])"),
-      new AbstractMap.SimpleEntry<>("Function with a subset parameter and a simple parameter", "A(B[,2], C)"),
-      new AbstractMap.SimpleEntry<>("Function with a function invocation as parameter", "A(B())"),
-      new AbstractMap.SimpleEntry<>("Function with multiple parameters", "A(B, C)"),
-      new AbstractMap.SimpleEntry<>("Function with multiple kinds of parameters", "A(B, C(), D, E(F(G/H)), A + B * C())"),
-      new AbstractMap.SimpleEntry<>("Function with formula as argument", "glm(A ~ B + C:D, poisson)"),
-      new AbstractMap.SimpleEntry<>("Function with named argument", "A(arg=x,another=y)"),
-      new AbstractMap.SimpleEntry<>("Function with string argument", "A('this')"),
-      new AbstractMap.SimpleEntry<>("Function with double quoted string argument", "A(\"this\")"),
-      new AbstractMap.SimpleEntry<>("Function with numerical string argument", "A('123')"),
-      new AbstractMap.SimpleEntry<>("Function with alphanumerical string argument and some special characters", "A('this1_that.this-that1')"),
-      new AbstractMap.SimpleEntry<>("Function with a list of colon separated numerical string argument", "A('1.2:1.3:-8.5')"),
-      new AbstractMap.SimpleEntry<>("Function with a list of comma separated numerical string argument", "A('1.2,1.3,-8.5')"),
-      new AbstractMap.SimpleEntry<>("Function with a list of semi colon separated numerical string argument", "A('1.2;1.3;-8.5')"),
-      new AbstractMap.SimpleEntry<>("Function with data frame column name string argument", "A('D$abc')"),
-      new AbstractMap.SimpleEntry<>("Function with data frame index string argument", "A('D[123]')"))
-      .collect(Collectors.toMap(AbstractMap.SimpleEntry::getKey, AbstractMap.SimpleEntry::getValue)));
+  private static final TestCase[] tests = new TestCase[]{
+      new TestCase("A symbol", "A"),
+      new TestCase("A number", "1.0"),
+      new TestCase("An integer", "5L"),
+      new TestCase("A negative number", "-0.43151402098822"),
+      new TestCase("An embedded symbol", "A$B$C.D"),
+      new TestCase("A subset symbol", "A[2, 1]"),
+      new TestCase("A subset symbol with range", "A[2, 1:2]"),
+      //new TestCase("An open row subset symbol", "A[,1]"),
+      //new TestCase("An open column subset symbol", "A[1,]"),
+      //new TestCase("A subset symbol with range", "A[,1:2]"),
+      //new TestCase("A subset symbol with function call", "D[,func(D[,1])]", "D[,dsBase::func(D[, 1])]"),
+      new TestCase("An empty subset symbol", "A[]"),
+      //new TestCase("An almost empty subset symbol", "A[,]"),
+      //new TestCase("A subset value symbol", "A[[1]]"),
+      new TestCase("A formula", "A ~ B", "A~B"),
+      new TestCase("A function invocation", "A()", "dsBase::A()"),
+      new TestCase("An operator on symbols", "A + B", "base::'+'(A, B)"),
+      new TestCase("Operator chaining", "A + B * C", "base::'+'(A, base::'*'(B, C))"),
+      new TestCase("Operator on functions", "A() + B * C", "base::'+'(dsBase::A(), base::'*'(B, C))"),
+      new TestCase("A formula with operators", "A ~ B + (C * D)^4 : E %in% F", "A~B+(C*D)^4:E%in%F"),
+      new TestCase("Grouping", "(A + B) * (C - D)", "base::'*'(base::'+'(A, B), base::'-'(C, D))"),
+      new TestCase("Function with a single parameter", "A(B)", "dsBase::A(B)"),
+      new TestCase("Function with a subset parameter", "A(B[,2])", "dsBase::A(B[2])"),
+      new TestCase("Function with a subset parameter and a simple parameter", "A(B[,2], C)", "dsBase::A(B[2], C)"),
+      new TestCase("Function with a function invocation as parameter", "A(B())", "dsBase::A(dsBase::B())"),
+      new TestCase("Function with multiple parameters", "A(B, C)", "dsBase::A(B, C)"),
+      new TestCase("Function with multiple kinds of parameters", "A(B, C(), D, E(F(G/H)), A + B * C())", "dsBase::A(B, dsBase::C(), D, dsBase::E(dsBase::F(base::'/'(G, H))), base::'+'(A, base::'*'(B, dsBase::C())))"),
+      new TestCase("Function with formula as argument", "glm(A ~ B + C:D, poisson)", "dsBase::glm(A~B+C:D, poisson)"),
+      new TestCase("Function with named argument", "A(arg=x,another=y)", "dsBase::A(arg = x, another = y)"),
+      new TestCase("Function with string argument", "A('this')", "dsBase::A('this')"),
+      new TestCase("Function with double quoted string argument", "A(\"this\")", "dsBase::A(\"this\")"),
+      new TestCase("Function with numerical string argument", "A('123')", "dsBase::A('123')"),
+      new TestCase("Function with alphanumerical string argument and some special characters", "A('this1_that.this-that1')", "dsBase::A('this1_that.this-that1')"),
+      new TestCase("Function with a list of colon separated numerical string argument", "A('1.2:1.3:-8.5')", "dsBase::A('1.2:1.3:-8.5')"),
+      new TestCase("Function with a list of comma separated numerical string argument", "A('1.2,1.3,-8.5')", "dsBase::A('1.2,1.3,-8.5')"),
+      new TestCase("Function with a list of semi colon separated numerical string argument", "A('1.2;1.3;-8.5')", "dsBase::A('1.2;1.3;-8.5')"),
+      new TestCase("Function with data frame column name string argument", "A('D$abc')", "dsBase::A('D$abc')"),
+      new TestCase("Function with data frame index string argument", "A('D[123]')", "dsBase::A('D[123]')")};
 
   @Test
   public void test_testCases() throws ParseException {
-    for (String msg : tests.keySet()) {
-      doTest(msg, tests.get(msg));
+    for (TestCase testCase : tests) {
+      doTest(testCase.getMsg(), testCase);
     }
-
   }
 
   @Test(expected = TokenMgrError.class)
@@ -114,87 +112,63 @@ public class DataShieldExprTest {
     doTest("Function with mixed quotes in string argument", "A(\"that')");
   }
 
-  private void doTest(String msg, String test) throws ParseException {
-    System.out.println(msg + ": " + test);
-    DataShieldGrammar g = new DataShieldGrammar(new StringReader(test));
-    SimpleNode expr = g.root();
-    expr.dump("");
+  @Test
+  public void test_script() throws ParseException {
+    //doTestVisitor("A[,1]", "A[,1]");
   }
 
-  @Test
-  public void test_visitor() throws ParseException {
-    DataShieldGrammarVisitor visitor = new DataShieldGrammarVisitor() {
+  private void doTest(String msg, String test) throws ParseException {
+    doTest(msg, new TestCase(msg, test));
+  }
 
-      @Override
-      public Object visit(ASTsymbol node, Object data) {
-        StringBuilder sb = (StringBuilder) data;
-        sb.append(node.value);
-        return data;
-      }
+  private void doTest(String msg, TestCase testCase) throws ParseException {
+    System.out.println(msg + ": " + testCase.getTest());
+    DataShieldGrammar g = new DataShieldGrammar(new StringReader(testCase.getTest()));
+    SimpleNode expr = g.root();
+    expr.dump("");
+    doTestVisitor(testCase.getTest(), testCase.getExpected());
+  }
 
+  private void doTestVisitor(String test, String expected) throws ParseException {
+    DSEnvironment environment = new DefaultDSEnvironment(null) {
       @Override
-      public Object visit(ASTstring node, Object data) {
-        StringBuilder sb = (StringBuilder) data;
-        sb.append(node.value);
-        return data;
-      }
-
-      @Override
-      public Object visit(ASTfuncCall node, Object data) {
-        StringBuilder sb = (StringBuilder) data;
-        sb.append(node.value).append("( ");
-        for (int i = 0; i < node.jjtGetNumChildren(); i++) {
-          Node child = node.jjtGetChild(i);
-          if (i > 0) sb.append(' ');
-          child.jjtAccept(this, sb);
-        }
-        sb.append(" )");
-        return sb;
-      }
-
-      @Override
-      public Object visit(ASTsubsetCall node, Object data) {
-        StringBuilder sb = (StringBuilder) data;
-        sb.append(node.value).append("[ ");
-        for (int i = 0; i < node.jjtGetNumChildren(); i++) {
-          Node child = node.jjtGetChild(i);
-          if (i > 0) sb.append(' ');
-          child.jjtAccept(this, sb);
-        }
-        sb.append(" ]");
-        return sb;
-      }
-
-      @Override
-      public Object visit(ASTBinaryOp node, Object data) {
-        StringBuilder sb = (StringBuilder) data;
-        sb.append(node.value).append("( ");
-        for (int i = 0; i < node.jjtGetNumChildren(); i++) {
-          Node child = node.jjtGetChild(i);
-          if (i > 0) sb.append(' ');
-          child.jjtAccept(this, sb);
-        }
-        sb.append(" )");
-        return sb;
-      }
-
-      @Override
-      public Object visit(ASTroot node, Object data) {
-        StringBuilder sb = (StringBuilder) data;
-        node.childrenAccept(this, data);
-        return sb;
-      }
-
-      @Override
-      public Object visit(SimpleNode node, Object data) {
-        return node.childrenAccept(this, data);
+      public DSMethod getMethod(String name) {
+        return new PackagedFunctionDSMethod(name, "dsBase::" + name);
       }
     };
-
-    String test = "A %*% (B(C ~ A + (G+D)^2 : H, C * D()) * 1/F)";
+    RScriptGenerator visitor = new RScriptGenerator(environment);
     DataShieldGrammar g = new DataShieldGrammar(new StringReader(test));
-    StringBuilder b = (StringBuilder) g.root().jjtAccept(visitor, new StringBuilder());
-    System.out.println(test + " --> " + b);
+    String script = visitor.toScript(g.root());
+    Assert.assertEquals(expected, script);
+  }
 
+  private static class TestCase {
+    private final String msg;
+    private final String test;
+    private final String expected;
+
+    private TestCase(String msg, String test) {
+      this.msg = msg;
+      this.test = test;
+      this.expected = test;
+    }
+
+    private TestCase(String msg, String test, String expected) {
+      this.msg = msg;
+      this.test = test;
+      this.expected = expected;
+    }
+
+    public String getMsg() {
+      return msg;
+    }
+
+    public String getTest() {
+      return test;
+    }
+
+    public String getExpected() {
+      return expected;
+    }
   }
 }
